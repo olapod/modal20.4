4b4691b9a8451e35048114345625fe05980d01f5
Ola Podsiadlik <alecs@op.pl>
2019-02-23 15:35:53 +0100

    Finish task 20.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-        output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
+module.exports = (env) => {
+    const environment = env || 'production';
+    
+    return {
+            mode: environment,
+            entry: './containers/App.js',
+            output: {
+                path: path.resolve(__dirname, 'build'),
+                filename: 'app.' + environment + '.bundle.js'
+            },
     module: {
         rules: [
             {
@@ -25,5 +29,7 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    
+}
 };
