236f07eab4422a1ca60d0fe402e1585a38d18739
Ola Podsiadlik <alecs@op.pl>
2019-02-24 13:30:29 +0100

    Change function in Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import App from "../../containers/App";
 
 
-class Title extends React.Component {
-    render() {
-        return <div className={'title'}>
-          <h1>ToDoList</h1>
-          <p>Number of Task: {App.data.length}</p>
-        </div>   
-      }
-    }
+// class Title extends React.Component {
+//     render() {
+//         return <div className={'title'}>
+//           <h1>ToDoList</h1>
+//           <p>Number of Task: {App.data.length}</p>
+//         </div>   
+//       }
+//     }
+
+const Title = props =>
+    <div>
+        <h1>TO DO LIST</h1>
+        <p>Number of Tasks: {App.data.length}</p>
+    </div>;
 
     export default Title;
